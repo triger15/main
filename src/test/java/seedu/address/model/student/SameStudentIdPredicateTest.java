11644ccee6e914ae56ac8c5ac01a07796e5afcec
wenshei <wensheiong@gmail.com>
2018-10-02 17:36:05 +0800

    checkstyle changes

--- src/test/java/seedu/address/model/student/SameStudentIdPredicateTest.java
+++ src/test/java/seedu/address/model/student/SameStudentIdPredicateTest.java
@@ -1,11 +1,11 @@
 package seedu.address.model.student;
 
-import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashSet;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 public class SameStudentIdPredicateTest {
 
