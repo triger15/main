f42ab8ff0c9c5293adf56457c37ef7c1d2b0473e
plehr <caephler@gmail.com>
2018-10-06 19:16:21 +0800

    Create TutorialGroupBuilder, a utility class for testing TutorialGroups

---
+++ src/test/java/systemtests/CreateTutorialGroupSystemTest.java
@@ -0,0 +1,17 @@
+package systemtests;
+
+import org.junit.Test;
+
+import seedu.address.model.Model;
+
+public class CreateTutorialGroupSystemTest extends AddressBookSystemTest {
+
+    @Test
+    public void createTutorialGroup() {
+        Model model = getModel();
+
+
+
+    }
+
+}
