2048814f6dd9409dfe3607e8775f7f4a4d8122bb
plehr <caephler@gmail.com>
2018-10-06 19:44:29 +0800

    Change TutorialGroupMaster to not use static uids. Create first system test.

--- src/test/java/systemtests/CreateTutorialGroupSystemTest.java
+++ src/test/java/systemtests/CreateTutorialGroupSystemTest.java
@@ -2,7 +2,12 @@ package systemtests;
 
 import org.junit.Test;
 
+import seedu.address.logic.commands.CreateTutorialGroupCommand;
 import seedu.address.model.Model;
+import seedu.address.model.student.Student;
+import seedu.address.model.tutorialgroup.TutorialGroup;
+import seedu.address.testutil.TutorialGroupBuilder;
+import seedu.address.testutil.TypicalPersons;
 
 public class CreateTutorialGroupSystemTest extends AddressBookSystemTest {
 
@@ -10,8 +15,27 @@ public class CreateTutorialGroupSystemTest extends AddressBookSystemTest {
     public void createTutorialGroup() {
         Model model = getModel();
 
+        Student firstStudent = TypicalPersons.ALICE;
+        Student secondStudent = TypicalPersons.BOB;
 
+        TutorialGroupBuilder tgBuilder = new TutorialGroupBuilder();
+        TutorialGroup typicalTg = tgBuilder.build();
+        String command = "  " + CreateTutorialGroupCommand.COMMAND_WORD + " n/CS1101S Studio 04A " + "id/04a";
 
+        assertCommandSuccess(command, typicalTg, String.format(CreateTutorialGroupCommand.MESSAGE_SUCCESS, typicalTg));
+    }
+
+    /**
+     * Checks whether the assertion was correct.
+     */
+    private void assertCommandSuccess(String command, TutorialGroup expected, String expectedMessage) {
+        Model expectedModel = getModel();
+        expectedModel.addTutorialGroup(expected);
+        executeCommand(command);
+        assertApplicationDisplaysExpected("", expectedMessage, expectedModel);
+        assertSelectedCardUnchanged();
+        assertCommandBoxShowsDefaultStyle();
+        assertStatusBarUnchangedExceptSyncStatus();
     }
 
 }
